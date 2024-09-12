8608ac7a83f47d335b7fa0be12f5d8ff4ec2464b
emyejudsfjerg <160884187+emyejudsfjerg@users.noreply.github.com>
2024-09-12 12:20:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biologia',
+    'Por que os flamingos são cor de rosa?',
+    'Porque eles se alimentam de crustáceos ricos em carotenoides.'
+)
+
+criaCartao(
+    'Química',
+    'Por que as cebolas nos fazem chorar?',
+    'Elas liberam uma substância que,ao entrar em contato com os olhos,forma ácido sulfúrico leve.'
+)
+
+criaCartao(
+    'Biologia',
+    'a quanto anos os tubarões estão na terra?',
+    'Os tubarẽs estão na terra há cerca de 400 milhões de anos, sendo assim mais velhos que as arvores que estão há 350 milhões de anos.'
+)
+
+criaCartao(
+    'Fisica',
+    'Quantas estrelas existem na Via Láctea?',
+    'Estima-se que haja entre 100 e 400 bilhões de estrelas na Via Láctea.'
+)
