eeb00d80ac49350b8c1f67b18338f4fc5ed7c31b
emyejudsfjerg <160884187+emyejudsfjerg@users.noreply.github.com>
2024-09-26 12:16:09 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -21,3 +21,162 @@ criaCartao(
     'Quantas estrelas existem na Via Láctea?',
     'Estima-se que haja entre 100 e 400 bilhões de estrelas na Via Láctea.'
 )
+
+criaCartao(
+    'Biologia' ,
+    'O que são células',
+    'São as unidades básicas da vida em todos os seres vivos.',
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é a função da mitocôndria?',
+    'Produzir energia para a célula por meio da respiração celula',
+)
+
+criaCartao(
+    'Química',
+    'O que é um átomo?',  
+    'A menor unidade de um elemento químico.',
+)
+
+criaCartao(
+    'Química',
+    'O que é uma molécula?',
+    'Conjunto de dois ou mais átomos ligados quimicamente.',
+)
+
+criaCartao(
+    'Química',
+    'O que é pH?',
+    'Escala que mede a acidez ou basicidade de uma solução',
+)
+
+criaCartao(
+    'Matemática',
+   ' O que é um número primo?',
+   'Número divisível apenas por 1 e por ele mesmo',
+)
+
+criaCartao(
+    'Matemática',
+    'O que é um triângulo equilátero?',
+  'Triângulo com todos os lados e ângulos iguais.',
+)
+
+criaCartao(
+    'Matemática',
+    'Como se calcula a média aritmética?',  
+   'Somando todos os valores e dividindo pela quantidade de valores.',
+)
+
+criaCartao(
+    'Física',
+    'O que é a teoria da relatividade de Einstein?',
+    'A teoria diz que o espaço e o tempo são relativos e que a velocidade da luz é constante para todos os observadores, independentemente de seu movimento.',
+)
+
+criaCartao(
+    'Física',
+    'O que é a matéria escura?',  
+   'É uma forma invisível de matéria que compõe cerca de 85% do universo, mas não interage com a luz, tornando-a difícil de detectar.',
+)
+
+criaCartao(
+    'Física',
+    'Por que o tempo passa mais devagar próximo a um buraco negro?',
+   'A gravidade extrema de um buraco negro distorce o espaço-tempo, fazendo o tempo passar mais devagar para objetos que estão perto dele.',
+)
+
+criaCartao(
+    'História',
+    'O que foi a Guerra dos Cem Anos?',
+    'Conflito entre a Inglaterra e a França que durou 116 anos (1337–1453), motivado por disputas territoriais e sucessão ao trono francês.',
+)
+
+criaCartao(
+    'História',
+    'Quem foi Genghis Khan e por que ele foi importante?',
+    'Genghis Khan foi o fundador do Império Mongol, o maior império contíguo da história. Ele revolucionou táticas de guerra e unificou grandes áreas da Ásia.',
+)
+
+criaCartao(
+    'História',
+    'Por que o Império Romano caiu?',
+    'Diversos fatores, como invasões bárbaras, corrupção interna, crise econômica e sobrecarga militar, contribuíram para o colapso do Império Romano no século V.',
+    )
+
+criaCartao(
+    'Geografia',
+    'Por que a Antártica é o continente mais seco do mundo?',
+    'Embora coberta de gelo, a Antártica recebe pouquíssima precipitação, sendo tecnicamente um deserto.',
+
+)
+
+criaCartao(
+    'Geografia',
+    'Por que as auroras boreais ocorrem?', 
+    'As auroras são causadas pela interação de partículas solares carregadas com o campo magnético da Terra, gerando luzes no céu.',
+)
+
+criaCartao(
+    'Geografia',
+    'O que é o Círculo de Fogo do Pacífico?',
+    'Região em torno do Oceano Pacífico onde ocorrem a maioria dos terremotos e erupções vulcânicas do mundo devido ao movimento das placas tectônicas.',
+
+)
+
+criaCartao(
+    'Curiosidades Científicas',
+    'Por que o céu é azul?',
+    'A luz do sol se espalha na atmosfera, e o azul é dispersado mais facilmente.',
+)
+
+criaCartao(
+    'Curiosidades Científicas',
+    'Quanto tempo a luz do Sol demora para chegar à Terra?',
+    'Cerca de 8 minutos.',
+)
+
+criaCartao(
+    'Curiosidades Científicas',
+    'Qual é o animal mais rápido do mundo?',
+    'O falcão-peregrino, em mergulho, pode atingir 320 km/h.',
+)
+
+criaCartao(
+    'Curiosidades Científicas',
+    'O que é a gravidade?',
+    'Força que atrai os objetos com massa uns aos outros.',
+)
+
+criaCartao(
+    'Curiosidades Científicas',
+    'Quantos ossos tem o corpo humano?',
+    'Um adulto tem 206 ossos.',
+)
+
+criaCartao(
+    'Curiosidades Científicas',
+    'Por que o gelo flutua na água?',
+    'Porque é menos denso que a água líquida.',
+
+)
+
+criaCartao(
+    'Curiosidades Científicas',
+    'O que causa o arco-íris?',
+    'Refração, reflexão e dispersão da luz solar em gotas de chuva.',
+)
+
+criaCartao(
+    'Curiosidades Científicas',
+    'Qual é o maior órgão do corpo humano?',
+    'A pele',
+)
+
+criaCartao(
+    'Curiosidades Científicas',
+    'O que são buracos negros?',  
+    'Regiões do espaço com gravidade tão forte que nada pode escapar delas.',
+)
